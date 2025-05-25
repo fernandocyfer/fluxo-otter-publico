6e023f093b824f81b1d4fe79cf8454f261172b5f
Fernando Filho <fernandofilho@MacBook-Pro-de-Fernando.local>
2025-05-25 15:01:55 -0300

    first commit

---
+++ fluxo-ottero-publico.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name: Fluxo Otter Publico
+ * Plugin URI: https://www.cyfer.com.br/fluxo-ottero-publico/
+ * Description: Fluxo Ottero Publico
+ * Version: 1.0.0
+ * Author: Cyfer Development
+ * Author URI: https://www.cyfer.com.br/
+ * Contributors: cyferweb
+ * Requires at least: 5.0
+ * Tested up to: 6.8
+ * Stable tag: 1.0.3
+ * License: GPLv2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: fluxo-otter-publico
+ */
+
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
